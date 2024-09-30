b7c5bf2426ed728de9442088664c691d10910d04
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-09-30 12:31:45 -0600

    CI/CD test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1 className={styles.title}>The start of Nessi Fishing Supply&#39;s dev environment. I should be orange!</h1>
+        <h1 className={styles.title}>The start of Nessi Fishing Supply&#39;s dev environment. I should be orange.</h1>
       </main>
     </div>
   );
