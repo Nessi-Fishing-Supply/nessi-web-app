2a8c5461abc993a934d88a3559c407f9ad37ad15
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-09-30 15:11:35 -0600

    test cache invalidation CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1 className={styles.title}>The start of Nessi Fishing Supply&#39;s dev environment. I should be orange! Testing name update.</h1>
+        <h1 className={styles.title}>The start of Nessi Fishing Supply&#39;s dev environment. I should be orange! Cache invalidation in Ci/CD</h1>
       </main>
     </div>
   );
