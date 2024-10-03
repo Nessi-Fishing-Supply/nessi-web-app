5fbcd835a29ffb47ce6d05d9bc9824aed8f4c057
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-10-02 19:23:20 -0600

    fix merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,10 @@ export default function Home() {
   return (
     <div>
       <main>
-<<<<<<< HEAD
-        <h1 className={styles.title}>The start of Nessi Fishing Supply&#39;s staging environment. I should be Yellow!</h1>
-=======
         <Navbar />
         <Link href="/marketplace">
           Go to Marketplace
         </Link>
->>>>>>> dev
       </main>
     </div>
   );
