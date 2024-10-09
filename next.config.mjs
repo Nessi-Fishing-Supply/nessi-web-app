2fde5354b11e5ff9e4eebc98cf26fdbcd9f87a0f
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-10-09 09:42:45 -0600

    update next configs path for styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Construct `__dirname` in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
     reactStrictMode: true,
     sassOptions: {
         sourceMap: true,
+        includePaths: [path.join(__dirname, 'src/styles')],
     },
     output: 'export',
-    
+
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
@@ -18,6 +26,15 @@ const nextConfig = {
                 },
             ],
         });
+
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@images': path.resolve(__dirname, 'src/assets/images'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos'),
+        };
 
         return config;
     },
