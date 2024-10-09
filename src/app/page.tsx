ae7bf3d115af865bb7a3c6563b573eaac5d6ae02
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-10-09 14:23:13 -0600

    CI/CD test 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Home</h1>
+        <h1>Home - Dev</h1>
         <Link href="/components">View Components</Link>
         <Link href="/forgot-password">Forgot Password</Link>
         <Link href="/reset-password">Reset Password</Link>
